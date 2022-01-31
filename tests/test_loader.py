ee01156225652a0bf43ebb3caa67668c52a1746c
Sriram Krishna <ksri91@gmail.com>
2022-01-31 20:27:59 +0530

    🧪 load user weighted wordlist

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -13,7 +13,12 @@ def test_custom():
     assert len([word for word in wl if word.weight == 1]) == 1318
 
 
-def test_file():
+def test_file_1():
     wl = load(length=None, filename="tests/ef3000.txt")
     assert len(wl) == 2994
     assert all([word.weight == 1 for word in wl])
+
+
+def test_filt_2():
+    wl = load(length=5, filename="tests/testwl.txt")
+    assert len(wl) == 1099
