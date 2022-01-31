f5b1ebdc5a64103231c19e819acd0aee5ee11bd3
Sriram Krishna <ksri91@gmail.com>
2022-01-31 23:51:40 +0530

    🧪 hard mode for play session

--- tests/test_playsession.py
+++ tests/test_playsession.py
@@ -1,6 +1,7 @@
+import pytest
 from wordle import loaders
-from wordle.core import PlaySession
-from wordle.wordle import Clue
+from wordle.core import HardModeInvalid, PlaySession
+from wordle.wordle import Clue, Wordle
 
 
 def test_playsession():
@@ -14,3 +15,22 @@ def test_playsession():
     ps.new()
     assert ps.tries == 0
     assert ps.answer() in wl.words
+
+
+def test_playsession_hard():
+    wl = loaders.load_wordlelist(filename="tests/ef3000.txt")
+    ans = "white"
+    ps = PlaySession(wl, mode="hard", ans=ans)
+
+    assert ans in wl.words
+    assert ps.answer() == ans
+
+    guess = Wordle("ahead")
+    assert ps.guess(guess) == Clue(guess, [0, 2, 1, 0, 0])
+
+    with pytest.raises(HardModeInvalid) as e:
+        ps.guess("bread")
+    assert e.value.args[1] == {"h"}
+
+    with pytest.raises(ValueError):
+        ps.new("fails")
