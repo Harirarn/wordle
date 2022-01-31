91b682fe541e45a48efd3b9dafd5e72cb2ebdb63
Sriram Krishna <ksri91@gmail.com>
2022-01-31 23:10:43 +0530

    🧪 HardModeFilter

--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,5 +1,4 @@
 import pytest
-
 import wordle.wordle as wordle
 
 
@@ -21,3 +20,26 @@ def test_compare(word, key, expected):
             wordle.compare(word, key)
     else:
         assert wordle.compare(word, key) == expected
+
+
+def test_hardmodefilter():
+    filt = wordle.HardModeFilter()
+    filt.add_clue(wordle.Clue("raise", [0, 1, 0, 0, 2]))
+
+    assert filt.test("plate") == set()
+    assert filt.test("sagol") == {"e"}
+
+    filt.add_clue(wordle.Clue("plain", [1, 0, 2, 0, 0]))
+
+    assert filt.test("agape") == set()
+    assert filt.test("apart") == {"e"}
+    assert filt.test("spite") == {"a"}
+    assert filt.test("sagol") == {"e", "p"}
+
+    filt.reset()
+
+    assert filt.test("sagol") == set()
+
+    filt.add_clue(wordle.Clue("balls", [0, 0, 1, 2, 0]))
+
+    assert filt.test("bails") == set()
