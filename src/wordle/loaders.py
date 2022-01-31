d9836a076108c974d98aa9ed15b427d8a7c8b317
Sriram Krishna <ksri91@gmail.com>
2022-01-31 20:29:50 +0530

    🐛 Avoid loading duplicate words

--- src/wordle/loaders.py
+++ src/wordle/loaders.py
@@ -25,7 +25,9 @@ def load_wordlelist(length: int | None = 5, filename: str = None) -> WordleList:
         wordlist = load_resource("custom")
     else:
         wordlist = load_file(filename)
-    return WordleList(filter_length(wordlist, length))
+    wordlist = filter_length(wordlist, length)
+    wordlist = dedup(wordlist)
+    return WordleList(wordlist)
 
 
 def load_file(filename: str | Path) -> list[tuple[str, int]]:
@@ -36,14 +38,15 @@ def load_file(filename: str | Path) -> list[tuple[str, int]]:
         wordlist = [
             (word.split(" ")[0], int(word.split(" ")[1]))
             for word in data
-            if word[0].isalpha()
+            if word != "" and word.split(" ")[0].isalpha()
         ]
     elif re.match(r"^\d+ \w+$", data[0]):
         wordlist = [
             (word.split(" ")[1], int(word.split(" ")[0]))
             for word in data
-            if word[0].isalpha()
+            if word != "" and word.split(" ")[1].isalpha()
         ]
+        pass
     else:
         raise ValueError("Invalid file format")
     return wordlist
@@ -56,22 +59,22 @@ def load_resource(source: str) -> list[tuple[str, int]]:
     commons = pkg_resources.read_text(RESOURSES, WORDLISTS[source]["commons"]).split(
         "\n"
     )
-    commonset = set(commons)
-    commonset.remove("")
-    allset = set(alls) - commonset
-    allset.remove("")
-    return [(word, 0) for word in allset] + [(word, 1) for word in commonset]
+    return [(word, 0) for word in alls] + [(word, 1) for word in commons]
 
 
 def filter_length(
-    wordlist: list[tuple[str, int]] | list[str], length: int | None
-) -> list[tuple[str, int]] | list[str]:
+    wordlist: list[tuple[str, int]], length: int | None
+) -> list[tuple[str, int]]:
     if length is None:
         return wordlist
-    if isinstance(wordlist[0], str):
-        return [word for word in wordlist if len(word) == length]  # type: ignore
-    return [word for word in wordlist if len(word[0]) == length]  # type: ignore
+    return [word for word in wordlist if len(word[0]) == length]
 
 
-def prune_non_alphabetic(wordlist: list[str]) -> list[str]:
-    return [word for word in wordlist if word.isalpha()]
+def dedup(wl: list[tuple[str, int]]) -> list[tuple[str, int]]:
+    wldict: dict[str, int] = {}
+    for word, weight in wl:
+        if word in wldict:
+            wldict[word] += weight
+        else:
+            wldict[word] = weight
+    return [(word, weight) for word, weight in wldict.items()]
