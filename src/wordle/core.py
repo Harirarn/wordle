5bd7e6eff11f27faafa99a2de9b548f0a81ce9be
Sriram Krishna <ksri91@gmail.com>
2022-02-01 00:52:35 +0530

    ✨ ✅ hard mode for play session

--- src/wordle/core.py
+++ src/wordle/core.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 import random
-from typing import Protocol
+from typing import Literal, Protocol
 
-from wordle.wordle import Clue, Wordle, WordleList
+from wordle.wordle import Clue, HardModeFilter, Wordle, WordleList
 
 
 class WordleSolver(Protocol):
@@ -26,22 +26,45 @@ class WordleSolver(Protocol):
         ...
 
 
-class PlaySession:
-    def __init__(self, wordlist: WordleList):
-        self.masterlist = wordlist
-        self.new()
+class HardModeInvalid(Exception):
+    pass
 
-    def new(self):
-        self.choice = random.choices(
-            self.masterlist.words, weights=self.masterlist.weights
-        )[0]
+
+class PlaySession:
+    def __init__(
+        self,
+        wordlist: WordleList,
+        mode: Literal["easy", "hard"] = "easy",
+        ans: Wordle | str | None = None,
+    ):
+        self.masterlist = wordlist
+        self.hard_filter = HardModeFilter()
+        self.mode = mode
+        self.new(ans)
+
+    def new(self, ans: Wordle | str | None = None):
+        if ans is not None:
+            if ans not in self.masterlist.words:
+                raise ValueError("Invalid answer")
+            self.choice = Wordle(ans)
+        else:
+            self.choice = random.choices(
+                self.masterlist.words, weights=self.masterlist.weights
+            )[0]
         self.tries = 0
+        self.hard_filter.reset()
 
     def guess(self, guess: str | Wordle) -> Clue:
         if guess not in self.masterlist.words:
             raise ValueError("Invalid guess")
+        if self.mode == "hard" and (missing := self.hard_filter.test(guess)):
+            raise HardModeInvalid(
+                f"Invalid guess for hard mode. Must contain {missing}", missing
+            )
         self.tries += 1
-        return Clue.from_compare(guess, self.choice)
+        clue = Clue.from_compare(guess, self.choice)
+        self.hard_filter.add_clue(clue)
+        return clue
 
     def answer(self) -> Wordle:
         return self.choice
