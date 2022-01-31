9105d82b367a847defd5a72fb96661c31c1c6a1a
Sriram Krishna <ksri91@gmail.com>
2022-01-31 17:49:19 +0530

    ✨ PlaySession

--- src/wordle/core.py
+++ src/wordle/core.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import random
 from typing import Protocol
 
 from wordle.wordle import Clue, Wordle, WordleList
@@ -23,3 +24,24 @@ class WordleSolver(Protocol):
 
     def list(self) -> list[Wordle]:
         ...
+
+
+class PlaySession:
+    def __init__(self, wordlist: WordleList):
+        self.masterlist = wordlist
+        self.new()
+
+    def new(self):
+        self.choice = random.choices(
+            self.masterlist.words, weights=self.masterlist.weights
+        )[0]
+        self.tries = 0
+
+    def guess(self, guess: str | Wordle) -> Clue:
+        if guess not in self.masterlist.words:
+            raise ValueError("Invalid guess")
+        self.tries += 1
+        return Clue.from_compare(guess, self.choice)
+
+    def answer(self) -> Wordle:
+        return self.choice
