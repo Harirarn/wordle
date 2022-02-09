e1f04ee9f12a2abb7c83a625a7e3aa484e2dce15
Sriram Krishna <ksri91@gmail.com>
2022-02-10 00:55:03 +0530

    ✨ store benchmark log data for future analysis

--- src/wordle/benchmark.py
+++ src/wordle/benchmark.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from typing import Type
 
 from wordle import loaders, solvers
@@ -7,15 +9,24 @@ from wordle.wordle import Wordle, WordleList
 history_depth = 2
 
 
-def calc_average_score(wordlist: WordleList, solvertype: Type[WordleSolver]) -> float:
+def calc_average_score(
+    wordlist: WordleList,
+    solvertype: Type[WordleSolver],
+    logfile: str | Path | None = None,
+) -> float:
     player = PlaySession(wordlist)
     solver = solvertype(wordlist)
     solver.reset()
     scores: list[int] = []
-    history: dict[tuple[tuple[Wordle, tuple[int, ...]], ...], Wordle] = {}
+    data = {}
+    history: dict[tuple[tuple[Wordle, tuple[int, ...]], ...], tuple[Wordle, float]] = {}
     solvewords = tuple(word.word for word in wordlist if word.weight > 0)
     totalwords = len(solvewords)
     for iword, word in enumerate(solvewords):
+        data[str(word)] = {
+            "tries": 0,
+            "guesses": [],
+        }
         player.new(word)
         solver.reset()
         solved = False
@@ -23,18 +34,25 @@ def calc_average_score(wordlist: WordleList, solvertype: Type[WordleSolver]) -> 
         print(f"{iword + 1:4d}/{totalwords}: {word}", end="\r", flush=True)
         while not solved:
             if tuple(curhist) in history:
-                guess = history[tuple(curhist)]
+                guess, points = history[tuple(curhist)]
             else:
-                guess = solver.besth(1)[0][0]
+                guess, points = solver.besth(1)[0]
                 if len(curhist) < history_depth:
-                    history[tuple(curhist)] = guess
+                    history[tuple(curhist)] = guess, points
             clue = player.guess(guess)
+            data[str(word)]["guesses"].append(  # type: ignore
+                (guess, points, tuple(clue))
+            )
             if all(i == 2 for i in clue):
                 solved = True
                 scores.append(player.tries)
+                data[str(word)]["tries"] = player.tries
                 continue
             solver.add_clue(clue)
             curhist.append((guess, tuple(clue)))
+    if logfile is not None:
+        with open(logfile, "w") as f:
+            json.dump(data, f)
     return sum(scores) / len(scores)
 
 
@@ -58,7 +76,8 @@ if __name__ == "__main__" or True:
         args = parse()
         solver = solvers.solversdict[args.solver]
         wordlist = loaders.load_wordlelist()
-        score = calc_average_score(wordlist, solver)
+        logfile = Path(f"benchmark_{args.solver}.json")
+        score = calc_average_score(wordlist, solver, logfile)
         print(f"For solver: {args.solver} average score: {score:.2f}")
 
     main()
